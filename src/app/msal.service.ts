d6c85715fbb420e89d98270fa36754ae9a157567
Gilberto <gilberto@sedep.com.br>
2023-04-25 10:36:17 -0400

    fix

--- src/app/msal.service.ts
+++ src/app/msal.service.ts
@@ -5,7 +5,6 @@ import { Subject } from 'rxjs';
 import { Profile } from './profile.model';
 const GRAPH_ENDPOINT = 'https://graph.microsoft.com/v1.0/me';
 const GRAPH_ENDPOINT_PIC = 'https://graph.microsoft.com/v1.0/me/photo/$value';
-const REPORTS_API_BASE_URI='https://localhost:44320/api/'
 
 @Injectable({
   providedIn: 'root'
@@ -21,14 +20,5 @@ export class MsalLoginService {
   {
     return this.httpClient.get(GRAPH_ENDPOINT_PIC,
       {responseType:'blob'});
-  }
-  getReport()
-  {
-    return this.httpClient.get(REPORTS_API_BASE_URI+'Report/GetReport',
-      {responseType:'blob'});
-  }
-  getReportStatus()
-  {
-    return this.httpClient.get<any>(REPORTS_API_BASE_URI+'Report/GetReportStatus');
   }
 }
